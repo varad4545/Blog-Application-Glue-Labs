b6eff45b335c6eb11bc686ffcc2ae541293a7bdd
varad4545 <godvarad@gmail.com>
2022-06-08 16:22:19 +0530

    first commit

---
+++ database/seeders/20220608054002-blogposts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('blogposts', [{
+      id:5,
+      email: "mohan@exmaple.com",
+      post: '[{"title":"Blog1","post":"mohans first blog"}]',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
